07fbd45e7491ca26292c3e18283ac9a486edb1f7
Artem melanich <pkeeper.shark@mail.ru>
2011-09-08 11:25:45 +0300

    include project to app address in INSTALLED_APPS

--- homework/settings.py
+++ homework/settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'profiles',
+    'homework.profiles',
 )
 
 # A sample logging configuration. The only tangible logging
