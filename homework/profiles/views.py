1f1e3aada25e0b974840fc39447455412cf62e98
Artem melanich <pkeeper.shark@mail.ru>
2011-09-08 16:39:27 +0300

    ticket:1 fix view

--- homework/profiles/views.py
+++ homework/profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render_to_response
 from homework.profiles.models import Profile, ContactField
 
 def show_profile(request, template_name="home.html"):
-    profile = get_object_or_404(Profile(pk=1))
+    profile = get_object_or_404(Profile,pk=1)
     contacts = ContactField.objects.filter(owner=profile)
     return render_to_response(template_name,{'profile':profile,
                                              'contacts':contacts})
