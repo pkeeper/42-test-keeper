424c44886132057ca932e32256d5fa882365bd25
Artem melanich <pkeeper.shark@mail.ru>
2011-09-08 16:36:11 +0300

    ticket:1 non relative python addresess, I'll it fix later

--- homework/profiles/urls.py
+++ homework/profiles/urls.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('',
+urlpatterns = patterns('homework',
     url(r'^$', 'profiles.views.show_profile', name='show_profile'),
 )
--- homework/profiles/views.py
+++ homework/profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render_to_response
-from models import Profile, ContactField
+from homework.profiles.models import Profile, ContactField
 
 def show_profile(request, template_name="home.html"):
     profile = get_object_or_404(Profile(pk=1))
