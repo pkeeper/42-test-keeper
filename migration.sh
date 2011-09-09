108cd118b1ce4af2dee552ed7c5f0dc9fd024e4e
Artem melanich <pkeeper.shark@mail.ru>
2011-09-10 01:50:00 +0300

    P2 once more

--- migration.sh
+++ migration.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 MANAG=/homework/manage.py
-python $DIR$MANAG syncdb --noinput
-python $DIR$MANAG schemamigration profiles --initial
-python $DIR$MANAG migrate profiles --fake
-#python $DIR$MANAG schemamigration profiles --auto
-#python $DIR$MANAG migrate profiles
+#python $DIR$MANAG syncdb --noinput
+#python $DIR$MANAG schemamigration profiles --initial
+#python $DIR$MANAG migrate profiles --fake
+python $DIR$MANAG schemamigration profiles --auto
+python $DIR$MANAG migrate profiles
