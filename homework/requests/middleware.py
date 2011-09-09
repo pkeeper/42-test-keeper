c9d798308ea4481b0409ac41154e37a5509c4da4
Artem melanich <pkeeper.shark@mail.ru>
2011-09-09 17:15:28 +0300

    ticket:3 log only wanted headers

--- homework/requests/middleware.py
+++ homework/requests/middleware.py
@@ -4,11 +4,26 @@ from models import RequestEntry
 class RequestLogMeddleware():
     
     def process_request(self, request):
+        
+        # Log only wanted headers
+        headers = (
+                   'HTTP_ACCEPT_ENCODING',
+                   'HTTP_ACCEPT_LANGUAGE',
+                   'HTTP_HOST',
+                   'HTTP_REFERER',
+                   'HTTP_USER_AGENT',
+                   'REMOTE_ADDR')
+        
+        filtered_headers = {}
+        
+        for h in headers:
+            filtered_headers[h] = request.META[h]
+        
         req = RequestEntry(
                            path=request.get_full_path(),
                            method=request.method,
                            params=request.REQUEST,
-                           headers=request.META,
+                           headers=filtered_headers,
                            )
         req.save()
         
