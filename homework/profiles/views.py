6532a01107bbcbb98422f7b1b37028891bf1eae9
Artem melanich <pkeeper.shark@mail.ru>
2011-09-18 02:36:02 +0300

    ticket:6 submit form via ajaxForm

--- homework/profiles/views.py
+++ homework/profiles/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.contrib.auth.decorators import login_required
 from django.forms.models import modelformset_factory
+from django.http import HttpResponse
 from django.template import RequestContext
+from django.utils import simplejson
+
 from models import Profile, ContactField
 from forms import ProfileForm
 
 
 def show_profile(request, template_name="home.html"):
-    profile = get_object_or_404(Profile, pk=1)
-    contact_list = ContactField.objects.filter(owner=profile)
-    contacts = contact_list.exclude(contact_type='Other contacts')
-    other_contacts = contact_list.filter(contact_type='Other contacts')
+    profile=get_object_or_404(Profile, pk=1)
+    contact_list=ContactField.objects.filter(owner=profile)
+    contacts=contact_list.exclude(contact_type='Other contacts')
+    other_contacts=contact_list.filter(contact_type='Other contacts')
 
-    context_dict = {
+    context_dict={
                     'profile': profile,
                     'contacts': contacts,
                     'other_contacts': other_contacts,
@@ -25,22 +28,25 @@ def show_profile(request, template_name="home.html"):
 
 @login_required
 def edit_profile(request, template_name="profile_edit.html"):
-    profile = get_object_or_404(Profile, pk=1)
-    contact_list = ContactField.objects.filter(owner=profile)
-    ContactsFormSet = modelformset_factory(ContactField,
+    profile=get_object_or_404(Profile, pk=1)
+    contact_list=ContactField.objects.filter(owner=profile)
+    ContactsFormSet=modelformset_factory(ContactField,
                                            extra=2, exclude=('owner',))
 
-    if request.method == 'POST':
-        postdata = request.POST.copy()
-        profile_form = ProfileForm(postdata, instance=profile)
-        contact_forms = ContactsFormSet(postdata, queryset=contact_list)
-        if profile_form.is_valid() and contact_forms.is_valid():
-            profile_form.save()
-            contact_forms.save()
+    if request.method=='POST':
+        postdata=request.POST.copy()
+        if request.is_ajax :
+            return HttpResponse('Form submited!')
+        else:
+            profile_form=ProfileForm(postdata, instance=profile)
+            contact_forms=ContactsFormSet(postdata, queryset=contact_list)
+            if profile_form.is_valid() and contact_forms.is_valid():
+                profile_form.save()
+                contact_forms.save()
     else:
-        profile_form = ProfileForm(instance=profile)
-        contact_forms = ContactsFormSet(queryset=contact_list)
-    context_dict = {
+        profile_form=ProfileForm(instance=profile)
+        contact_forms=ContactsFormSet(queryset=contact_list)
+    context_dict={
                     'profile_form': profile_form,
                     'contatcs_forms': contact_forms,
                    }
