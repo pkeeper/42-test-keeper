f9af9d6eda39f9c41bd2bf64b0f1bfd49c9f1558
Artem melanich <pkeeper.shark@mail.ru>
2011-09-21 17:12:23 +0300

    merge changes from t1_contact

--- homework/profiles/tests.py
+++ homework/profiles/tests.py
@@ -11,6 +11,13 @@ class ProfileTest(TestCase):
     def setUp(self):
         self.profile = Profile.objects.get(pk=1)
 
+        contact_list = ContactField.objects.filter(owner=self.profile)
+
+        self.contacts = contact_list.exclude(contact_type='Other contacts')
+        self.other_cont = contact_list.filter(contact_type='Other contacts')[0]
+
+        self.response = self.client.get(reverse('show_profile'))
+
     def test_profile_model(self):
         self.assertEqual(self.profile.name, 'Artem')
         self.assertEqual(self.profile.surname, 'Melanich')
@@ -21,23 +28,29 @@ class ProfileTest(TestCase):
                                                                day=15))
 
     def test_contacts(self):
-        contacts = ContactField.objects.filter(owner=self.profile)
-        self.assertEqual(contacts.count(), 5)
-        self.assertTrue(contacts.get(uid='288877528',
+        self.assertEqual(self.contacts.count(), 4)
+        self.assertTrue(self.contacts.get(uid='288877528',
                                      contact_type='ICQ'))
-        self.assertTrue(contacts.get(uid='pkeeper@jabber.com.ua',
+        self.assertTrue(self.contacts.get(uid='pkeeper@jabber.com.ua',
                                      contact_type='Jabber'))
-        self.assertTrue(contacts.get(uid='pensivekeeper@gmail.com',
+        self.assertTrue(self.contacts.get(uid='pensivekeeper@gmail.com',
                                      contact_type='Jabber'))
-        self.assertTrue(contacts.get(uid='pkeeper.shark@mail.ru',
+        self.assertTrue(self.contacts.get(uid='pkeeper.shark@mail.ru',
                                      contact_type='Email'))
-        self.assertTrue(contacts.get(uid='Multiline',
-                                     contact_type='Other contacts'))
+        self.assertTrue(self.other_cont)
 
-    def test_rendering(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def test_profile_rendering(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertContains(self.response, self.profile.name, count=1)
+        self.assertContains(self.response, self.profile.surname, count=1)
+        self.assertContains(self.response, 'Sept. 15, 1988', count=1)
+        self.assertContains(self.response, self.profile.bio)
 
+    def test_profile_contacts_rendering(self):
+        for c in self.contacts:
+            c_rendered_html = '<p>' + c.contact_type + ': ' + c.uid + '</p>'
+            self.assertContains(self.response, c_rendered_html, 1)
+        self.assertContains(self.response, self.other_cont.uid)
 
 class ProfileEditTest(TestCase):
     fixtures = ['admin_data.json', ]
