ebb3214e25e6f46b770bdf080596db5407f84783
Artem melanich <pkeeper.shark@mail.ru>
2011-09-24 10:16:56 +0300

    ticket:5 formset validation

--- homework/profiles/forms.py
+++ homework/profiles/forms.py
@@ -1,7 +1,51 @@
 from django import forms
-from models import Profile
+from django.core.validators import validate_email
+from django.forms.models import BaseModelFormSet
+from models import Profile, ContactField
 
 
 class ProfileForm(forms.ModelForm):
+    """
+        Form for Profile model
+    """
     class Meta:
         model = Profile
+
+
+class ContactsFormSet(BaseModelFormSet):
+
+    def clean(self):
+        super(ContactsFormSet, self).clean()
+        """Checks that uid is valid for given contact type."""
+        if any(self.errors):
+            # Don't bother validating the formset unless each form is valid 
+            # on its own
+            return
+        for i in range(0, self.total_form_count()):
+            form = self.forms[i]
+            # Do not validate if object is deleting
+            if form in self.deleted_forms:
+                continue
+            # Do not validate empty extra forms
+            if not form.initial and not form.cleaned_data:
+                continue
+            uid = form.cleaned_data['uid']
+            contact_type = form.cleaned_data['contact_type']
+            if contact_type in ('Email', 'Jabber'):
+                    try:
+                        validate_email(uid)
+                    except:
+                        raise forms.ValidationError(uid + ' ' + contact_type +
+                                                    " is not valid")
+                        return False
+            if contact_type == "ICQ":
+                if not uid.isnumeric():
+                    raise forms.ValidationError(uid +
+                                                ' ICQ number is not valid')
+                    return False
+
+    def __init__(self, *args, **kwargs):
+        self.queryset = ContactField.objects.filter(owner=kwargs['profile'])\
+                                                   .order_by('pk')
+        del kwargs['profile']
+        super(BaseModelFormSet, self).__init__(*args, **kwargs)
--- homework/profiles/views.py
+++ homework/profiles/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.forms.models import modelformset_factory
 from django.template import RequestContext
 from models import Profile, ContactField
-from forms import ProfileForm
+from forms import ProfileForm, ContactsFormSet
 
 
 def show_profile(request, template_name="home.html"):
@@ -26,20 +26,24 @@ def show_profile(request, template_name="home.html"):
 @login_required
 def edit_profile(request, template_name="profile_edit.html"):
     profile = get_object_or_404(Profile, pk=1)
-    contact_list = ContactField.objects.filter(owner=profile)
-    ContactsFormSet = modelformset_factory(ContactField, can_delete=True,
+    CFormSet = modelformset_factory(ContactField, can_delete=True,
+                                           formset=ContactsFormSet,
                                            extra=2, exclude=('owner',))
 
     if request.method == 'POST':
         postdata = request.POST.copy()
         profile_form = ProfileForm(postdata, instance=profile)
-        contact_forms = ContactsFormSet(postdata, queryset=contact_list)
+        contact_forms = CFormSet(postdata, profile=profile)
         if profile_form.is_valid() and contact_forms.is_valid():
+            # Save changes in forms
             profile_form.save()
             contact_forms.save()
+            # Reload forms to display fresh data
+            profile_form = ProfileForm(instance=profile)
+            contact_forms = CFormSet(profile=profile)
     else:
         profile_form = ProfileForm(instance=profile)
-        contact_forms = ContactsFormSet(queryset=contact_list)
+        contact_forms = CFormSet(profile=profile)
     context_dict = {
                     'profile_form': profile_form,
                     'contatcs_forms': contact_forms,
