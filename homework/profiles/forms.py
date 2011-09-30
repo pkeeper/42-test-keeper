c77d03221a416dfbedfc915864ff3b4e5b8381b8
Artem melanich <pkeeper.shark@mail.ru>
2011-09-30 13:08:12 +0300

    merge changes from t9_command

--- homework/profiles/forms.py
+++ homework/profiles/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.utils.safestring import mark_safe
-from models import Profile
+from django.core.validators import validate_email
+from django.forms.models import BaseModelFormSet
+from django.forms.util import ErrorList
+from models import Profile, ContactField
 
 
 class CustomDateWidget(forms.DateInput):
@@ -32,3 +35,47 @@ class ProfileForm(forms.ModelForm):
         widgets = {
             'birthdate': CustomDateWidget(),
         }
+
+
+class ContactsFormSet(BaseModelFormSet):
+
+    def clean(self):
+        super(ContactsFormSet, self).clean()
+        """Checks that uid is valid for given contact type."""
+        form_is_valid = True
+        for i in range(0, self.total_form_count()):
+            form = self.forms[i]
+            # Do not validate if object is deleting
+            if getattr(self, 'deleted_forms', False):
+                if form in self.deleted_forms:
+                    continue
+            # Do not validate empty extra forms
+            if not getattr(form, 'cleaned_data', False):
+                continue
+            uid = form.cleaned_data['uid']
+            contact_type = form.cleaned_data['contact_type']
+            if contact_type in ('Email', 'Jabber'):
+                    try:
+                        validate_email(uid)
+                    except:
+                        if not getattr(form._errors, 'contact_type', False):
+                            form._errors['contact_type'] = ErrorList()
+                        form._errors['contact_type'].append(contact_type +
+                                                            " is not valid.")
+                        form_is_valid = False
+            if contact_type == "ICQ":
+                if not uid.isnumeric():
+                    if not getattr(form._errors, 'contact_type', False):
+                        form._errors['contact_type'] = ErrorList()
+                    form._errors['contact_type']\
+                                            .append('ICQ number is not valid.')
+                    form_is_valid = False
+        return form_is_valid
+
+    def __init__(self, *args, **kwargs):
+        queryset = ContactField.objects.filter(owner=kwargs['profile'])\
+                                                   .order_by('-pk')
+        if queryset is not None:
+            self.queryset = queryset
+        del kwargs['profile']
+        super(BaseModelFormSet, self).__init__(*args, **kwargs)
--- homework/profiles/tests.py
+++ homework/profiles/tests.py
@@ -2,8 +2,53 @@ import datetime
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 from django.forms.models import modelformset_factory
+from django.utils import simplejson
 from models import Profile, ContactField
-from forms import ProfileForm
+from forms import ProfileForm, ContactsFormSet
+
+
+test_datasetA = {
+        u'name': [u'Artem-test'],
+        u'surname': [u'Melanich-test'],
+        u'birthdate': [u'1988-09-25'],
+        u'bio': [u'Multiline-test'],
+        u'form-0-id': [u'1'],
+        u'form-0-contact_type': [u'Jabber'],
+        u'form-0-uid': [u'test@mail.ru'],
+        u'form-1-id': [u'2'],
+        u'form-1-uid': [u'22222222'],
+        u'form-1-contact_type': [u'ICQ'],
+        u'form-2-id': [u'3'],
+        u'form-2-DELETE': [u'on'],
+        u'form-3-id': [u'4'],
+        u'form-3-DELETE': [u'on'],
+        u'form-4-id': [u'5'],
+        u'form-4-DELETE': [u'on'],
+        u'form-TOTAL_FORMS': [u'7'],
+        u'form-INITIAL_FORMS': [u'5'],
+        u'form-MAX_NUM_FORMS': [u''], }
+
+test_datasetB = {
+        # With empty fields
+        u'name': [u''],
+        u'surname': [u''],
+        u'birthdate': [u'notDate'],
+        u'bio': [u''],
+        u'form-0-id': [u'1'],
+        u'form-0-contact_type': [u'ICQ'],
+        u'form-0-uid': [u'test@mail.ru'],
+        u'form-1-id': [u'2'],
+        u'form-1-uid': [u'22222222'],
+        u'form-1-contact_type': [u'Jabber'],
+        u'form-2-id': [u'3'],
+        u'form-2-contact_type': [u'Email'],
+        u'form-2-uid': [u'notEmail'],
+        u'form-3-id': [u'4'],
+        u'form-3-contact_type': [u'Other contacts'],
+        u'form-3-uid': [u''],
+        u'form-TOTAL_FORMS': [u'6'],
+        u'form-INITIAL_FORMS': [u'2'],
+        u'form-MAX_NUM_FORMS': [u''], }
 
 
 class ProfileShowTest(TestCase):
@@ -73,16 +118,76 @@ class ProfileEditTest(TestCase):
         self.response = self.c.get(reverse('edit_profile'))
         self.assertContains(self.response, form.as_p(), count=1,
                             status_code=200)
-
-        contact_list = ContactField.objects.filter(owner=self.profile)\
-                                                   .order_by('-pk')
-        ContactsFormSet = modelformset_factory(ContactField, extra=2,
-                                               can_delete=True,
-                                               fields=('uid', 'contact_type'))
-        contact_forms = ContactsFormSet(queryset=contact_list)
+        CFormSet = modelformset_factory(ContactField, extra=2,
+                                        formset=ContactsFormSet,
+                                        can_delete=True,
+                                        fields=('uid', 'contact_type'))
+        contact_forms = CFormSet(profile=self.profile)
         self.assertContains(self.response, contact_forms.management_form,
                             count=1)
         self.assertContains(self.response, contact_forms.as_p(), count=1)
+
+    def test_change(self):
+        self.c.login(username='admin', password='admin')
+        self.assertEqual(self.c.post(reverse('edit_profile'),
+                                     test_datasetA).status_code, 302)
+        self.profile = Profile.objects.get(pk=1)
+        queryset = ContactField.objects.filter(owner=self.profile)\
+                                                    .order_by('pk')
+        self.assertEqual(self.profile.name, 'Artem-test')
+        self.assertEqual(self.profile.surname, 'Melanich-test')
+        self.assertEqual(self.profile.bio, 'Multiline-test')
+        self.assertEqual(queryset.count(), 2)
+        self.assertEqual(queryset.get(pk=1).uid, 'test@mail.ru')
+        self.assertEqual(queryset.get(pk=1).contact_type, 'Jabber')
+        self.assertEqual(queryset.get(pk=2).uid, '22222222')
+
+    def test_validation(self):
+        self.c.login(username='admin', password='admin')
+        response = self.c.post(reverse('edit_profile'), test_datasetB)
+        self.assertContains(response, 'This field is required.',
+                            count=4)
+        self.assertContains(response, 'ICQ number is not valid.',
+                            count=1)
+        self.assertContains(response, 'Jabber is not valid.', count=1)
+        self.assertContains(response, 'Email is not valid.', count=1)
+
+
+class ProfileAjaxTest(TestCase):
+    fixtures = ['admin_data.json', ]
+
+    def setUp(self):
+        self.profile = Profile.objects.get(pk=1)
+        self.c = Client()
+
+    def test_submit(self):
+        self.c.login(username='admin', password='admin')
+        self.c.post(reverse('edit_profile'), test_datasetA,
+                                     HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.profile = Profile.objects.get(pk=1)
+        queryset = ContactField.objects.filter(owner=self.profile)\
+                                                    .order_by('pk')
+        self.assertEqual(self.profile.name, 'Artem-test')
+        self.assertEqual(self.profile.surname, 'Melanich-test')
+        self.assertEqual(self.profile.bio, 'Multiline-test')
+        self.assertEqual(queryset.count(), 2)
+        self.assertEqual(queryset.get(pk=1).uid, 'test@mail.ru')
+        self.assertEqual(queryset.get(pk=1).contact_type, 'Jabber')
+        self.assertEqual(queryset.get(pk=2).uid, '22222222')
+
+    def test_validation(self):
+        self.c.login(username='admin', password='admin')
+        response = self.c.post(reverse('edit_profile'), test_datasetB,
+                                     HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        errors = simplejson.loads(response.content)
+        self.assertEqual(errors['profile_errors']['name'][0],
+                            'This field is required.')
+        self.assertEqual(errors['contacts_errors'][0]['contact_type'][0],
+                            'ICQ number is not valid.')
+        self.assertEqual(errors['contacts_errors'][1]['contact_type'][0],
+                            'Jabber is not valid.')
+        self.assertEqual(errors['contacts_errors'][2]['contact_type'][0],
+                            'Email is not valid.')
 
 
 class DateWidgetTest(TestCase):
