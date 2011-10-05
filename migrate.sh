88e5c89cd1c90be95086565d7db291a7492e47ee
Artem melanich <pkeeper.shark@mail.ru>
2011-10-05 14:50:53 +0300

    rebuild

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sh ../uwsgi/post_deploy.sh
+
 python ./homework/manage.py sqlclear auth sites modelslog requests admin contenttypes profiles south | python ./homework/manage.py dbshell
 python ./homework/manage.py syncdb --noinput
 python ./homework/manage.py migrate
