8202ba6b28cd61e4bd3861f81eb65bc468589845
Artem melanich <pkeeper.shark@mail.ru>
2011-10-05 12:49:34 +0300

    remove old fixtures load

--- migrate.sh
+++ migrate.sh
@@ -2,5 +2,4 @@
 sh ../uwsgi/post_deploy.sh
 python ./homework/manage.py sqlclear auth sites modelslog requests admin contenttypes profiles | python ./homework/manage.py dbshell
 python ./homework/manage.py syncdb --noinput
-python ./homework/manage.py loaddata homework/Tinitial_data.json homework/profiles/fixtures/Tinitial_data.json
 exit 0
